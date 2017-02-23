8958eac93c3f9ad312793ecf0f8c1b902b9615cb
Kyle <kylekatarnls@gmail.com>
2017-02-23 08:27:57 +0000

    Apply fixes from StyleCI

--- src/Phug/ExtensionInterface.php
+++ src/Phug/ExtensionInterface.php
@@ -5,26 +5,42 @@ namespace Phug;
 interface ExtensionInterface
 {
     public function __construct(Phug $phug);
+
     // Contents
     public function getParameters();
+
     public function getMixins();
+
     public function getBlocks();
+
     // Lexer
     public function getTokens();
+
     public function getScanners();
+
     // Parser
     public function getNodes();
+
     public function getFilters();
+
     public function getHandlers();
+
     // Formatter
     public function getElements();
+
     public function getFormats();
+
     public function getAssignmentHandlers();
+
     public function getPatterns();
+
     public function getAttributeHandlers();
+
     // Compiler
     public function getPathResolvers();
+
     public function getTranslators();
+
     // Renderer
     public function getAdapters();
 }
