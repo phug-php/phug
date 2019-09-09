c2b57a246f37a2dc9d4a6ada712d2b5d2a1643b1
KyleKatarn <kylekatarnls@gmail.com>
2019-09-09 16:28:36 +0200

    Add missing stream_set_option method for PHP 7.4

--- src/Phug/Renderer/Renderer/Adapter/Stream/Template.php
+++ src/Phug/Renderer/Renderer/Adapter/Stream/Template.php
@@ -76,5 +76,19 @@ class Template
     {
         return [0, 0, 0, 0, 0, 0, 0, strlen($this->data), 0, 0, 0, 0];
     }
+
+    /**
+     * Dummy option setter.
+     *
+     * @param $option
+     * @param $arg1
+     * @param $arg2
+     *
+     * @return bool
+     */
+    public function stream_set_option($option, $arg1, $arg2)
+    {
+        return true;
+    }
 }
 // @codingStandardsIgnoreEnd
