a37d6da1538f992dbd83bbf4b530e75653e34f46
kylekatarnls <kylekatarnls@gmail.com>
2023-09-02 12:48:54 +0200

    Use strict in_array checks in formatter

--- src/Phug/Formatter/Formatter.php
+++ src/Phug/Formatter/Formatter.php
@@ -504,7 +504,7 @@ class Formatter implements ModuleContainerInterface
         }
 
         foreach ($this->mixins->getRequirementsStates() as $key => $value) {
-            if ($value || $this->mixinsAllRequired || in_array($key, $this->mixinsPreCalled)) {
+            if ($value || $this->mixinsAllRequired || in_array($key, $this->mixinsPreCalled, true)) {
                 $dependencies .= $this->mixins->get($key);
             }
         }
